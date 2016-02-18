984b08a3f99a1f35e59a6d44a0c580044020fac3
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-02-18 16:51:30 +0530

    [Dharam]  modified the way of implementation of Matrix class
    What changes have you made?
    
    Made all the data related to class private to that class. Because i don't
    want to take responsibility of take care of the wrong thing what others
    can do with the matrix object if i expose it's data to public

--- javaAssignment/matrix/Matrix.java
+++ javaAssignment/matrix/Matrix.java
@@ -8,17 +8,17 @@ public class Matrix {
 		this.row = row;
 		this.colomn = colomn;
 		this.matrix = new int[row][colomn];
-   }
-   public int row (){
-		return this.row;
-   }
-   public int colomn (){
-		return this.colomn;
-   }
-   public int[][] get(){
-		return matrix;
-   }
-   public void populate(int []values){
+   	}
+   	private int row (){
+   		return this.row;
+   	}
+   	private int colomn (){
+   		return this.colomn;
+   	}
+   	public boolean isEqualTo(int [][] matrixToBeCompared){
+		return Arrays.deepEquals(this.matrix,matrixToBeCompared);
+	}
+	public void assignedValues(int []values){
 		int count=0;
 		for(int i=0; i<this.row; i++){
 			for(int j=0; j<this.colomn; j++){
@@ -26,20 +26,21 @@ public class Matrix {
 				count++;
 			}
 		}
-   }
-   public void print(){
+	}
+	public void print(){
 		for(int i=0; i<this.row; i++){
-		for(int j=0; j<this.colomn; j++)
-			System.out.print("\t"+this.matrix[i][j]+"\t");
-		System.out.println();
+			for(int j=0; j<this.colomn; j++)
+				System.out.print("\t"+this.matrix[i][j]+"\t");
+			System.out.println();
 		}
-   }
-
-   public Matrix add(Matrix addendMatrix){
-      Matrix result = new Matrix(this.row,this.colomn);
-      for(int i=0; i<this.row; i++)
-         for(int j=0; j<this.colomn; j++)
-            result.matrix[i][j] = this.matrix[i][j]+addendMatrix.matrix[i][j];
-      return result;
+	}
+	public Matrix add(Matrix addendMatrix){
+		if(this.row!=addendMatrix.row||this.colomn!=addendMatrix.colomn)
+			return new Matrix(addendMatrix.row,addendMatrix.colomn);
+		Matrix result = new Matrix(this.row,this.colomn);
+		for(int i=0; i<this.row; i++)
+			for(int j=0; j<this.colomn; j++)
+				result.matrix[i][j] = this.matrix[i][j]+addendMatrix.matrix[i][j];
+		return result;
    }
 }
--- javaAssignment/test/MatrixTest.java
+++ javaAssignment/test/MatrixTest.java
@@ -1,74 +1,66 @@
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import javaAssignment.matrix.Matrix;
 
 
 public class MatrixTest {
     @Test
-    public void testForCreateMatrixOf2By3() {
-    	Matrix twoBy3 = new Matrix(2,3);
-    	assertEquals(twoBy3.row(),2);
-    	assertEquals(twoBy3.colomn(),3);
-    }
-    @Test
-    public void testForPopulate2By3Matrix() {
-    	Matrix twoBy3 = new Matrix(2,3);
+    public void assignedValuesShouldPopulateTheMatrixWithGivenValues() {
+    	Matrix twoBy3Matrix = new Matrix(2,3);
     	int [] values = {1,2,3,4,5,6};
+        twoBy3Matrix.assignedValues(values);
+
     	int [][] expected = {{1,2,3},{4,5,6}};
-    	twoBy3.populate(values);
-    	int [][] actual = twoBy3.get();
-    	assertArrayEquals(expected,actual);
+    	assertTrue(twoBy3Matrix.isEqualTo(expected));
     }
     @Test
-    public void testForPopulate4By4Matrix() {
-    	Matrix fourByFour = new Matrix(4,4);
-    	int [] values = new int[16];
+    public void assignedValuesShouldExcludeExtraValuesProvidedToAssign() {
+    	Matrix fourByFourMatrix = new Matrix(4,4);
+    	int [] values = new int[20];
     	for (int i=0;i<16;i++)
     		values[i] = i+1;
-        int [][] expected ={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    	fourByFour.populate(values);
-        assertArrayEquals(expected,fourByFour.get());
+        int [][] expected ={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20}};
+    	fourByFourMatrix.assignedValues(values);
+        assertTrue(!fourByFourMatrix.isEqualTo(expected));
     }
     @Test
-    public void testForAdditionOfTwoMatrices() {
+    public void addShouldAddTwoMatrixAndReturnedNewMatrix() {
         Matrix fourByFour1 = new Matrix(4,4);
         int [] values1 = new int[16];
         for (int i=0;i<16;i++)
             values1[i] = i+1;
-        fourByFour1.populate(values1);
+        fourByFour1.assignedValues(values1);
 
         Matrix fourByFour2 = new Matrix(4,4);
         int [] values2 = new int[16];
         for (int i=0;i<16;i++)
             values2[i] = i+1;
-        fourByFour2.populate(values2);
+        fourByFour2.assignedValues(values2);
 
         Matrix result = fourByFour1.add(fourByFour2);
 
         int [][] expected ={{2,4,6,8},{10,12,14,16},{18,20,22,24},{26,28,30,32}};
-        assertArrayEquals(expected,result.get());
+        assertTrue(result.isEqualTo(expected));
     }
 
     @Test
-    public void testForAdditionOfTwoMatricesWhereOneMatrixHasNegativeValues() {
+    public void addShouldReturnANullMatrixIfTwoMatrixAreOfDifferentOrder() {
         Matrix fourByFour1 = new Matrix(4,4);
         int [] values1 = new int[16];
         for (int i=0;i<16;i++)
             values1[i] = i+1;
-        fourByFour1.populate(values1);
+        fourByFour1.assignedValues(values1);
 
-        Matrix fourByFour2 = new Matrix(4,4);
-        int [] values2 = new int[16];
-        for (int i=0;i<16;i++)
-            values2[i] = -(i+1);
-        fourByFour2.populate(values2);
+        Matrix fourByFour2 = new Matrix(2,3);
+        int [] values2 = new int[6];
+        for (int i=0;i<6;i++)
+            values2[i] = i+1;
+        fourByFour2.assignedValues(values2);
 
         Matrix result = fourByFour1.add(fourByFour2);
-
-        int [][] expected ={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
-        assertArrayEquals(expected,result.get());
+        int [][] expected ={{0,0,0},{0,0,0}};
+        assertTrue(result.isEqualTo(expected));
     }
 
 }
